84c710bff15e535028a50d4e3287316f5075a658
tokoyi <tokoyi@gmail.com>
2021-09-19 13:25:57 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'TP APIs',
+  tagline: 'Frist try ...',
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
@@ -8,7 +8,7 @@ module.exports = {
   projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'TP APIs',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
