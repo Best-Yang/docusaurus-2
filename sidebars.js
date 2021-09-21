669225f4bddda23be23d679aefc11a436c23d37d
tokoyi <tokoyi@gmail.com>
2021-09-21 04:28:28 +0000

    update

--- sidebars.js
+++ sidebars.js
@@ -2,5 +2,24 @@ module.exports = {
   someSidebar: {
     Docusaurus: ['doc1', 'doc2', 'doc3'],
     Features: ['mdx'],
+    签名验证: ['signature'],
+    API: [
+      '获取项目列表', 
+      '获取指定项目基本信息',
+      '获取指定项目日志',
+      '获取总列表_指定工厂_未结束项目_限自研商品',
+      '获取指定项目物料列表_限自研商品',
+      '获取指定物料详情'
+    ],
+    传出WEBHOOK: [
+      '物料到货事件'
+    ],
+    传入WEBHOOK: [
+      '物料到货事件'
+    ],
+    工具: [
+      'Debug Tools（JS）',
+      'Debug Tools（PHP）'
+    ],
   },
 };
